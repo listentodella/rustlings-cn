6476df59c4d90bb4d3dfe8f36c3e1ea50d7d666b
leo <leo233.huang@gmail.com>
2024-03-02 17:21:15 +0800

    简化trait1.rs

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -15,7 +15,7 @@ trait AppendBar {
 impl AppendBar for String {
     // TODO: 为类型 `String` 实现 `AppendBar`。
     fn append_bar(self) -> Self {
-         self.to_owned() + "Bar"
+         self + "Bar"
     }
 }
 
