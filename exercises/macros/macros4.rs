d54ea10598ea2ae0a374188b8a2ea1851e6e4570
leo233 <leo233.huang@gmail.com>
2024-05-20 22:54:53 +0800

    宏练习4

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,10 +1,8 @@
 // macros4.rs
 // 执行 `rustlings hint macros4` 或在观察模式下使用 `hint` 子命令来获取提示。
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
-    (_) => {
+    () => {
         println!("Check out my macro!");
     };
     ($val:expr) => {
